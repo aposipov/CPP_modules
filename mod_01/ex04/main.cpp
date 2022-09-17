e2f6e0abdbe7e988b549a66ec6b32005b8484f0b
aposipov <a.p.osipov@ro.ru>
2022-09-17 23:26:29 +1000

    01 05

--- mod_01/ex04/main.cpp
+++ mod_01/ex04/main.cpp
@@ -46,11 +46,11 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	std::string in_file(argv[1]);
-	std::cout << in_file << std::endl;//
+//	std::cout << in_file << std::endl;//
 	std::string s1(argv[2]);
-	std::cout << s1 << std::endl;//
+//	std::cout << s1 << std::endl;//
 	std::string s2(argv[3]);
-	std::cout << s2 << std::endl;//
+//	std::cout << s2 << std::endl;//
 
 	if (in_file.empty() || s1.empty() || s2.empty())
 	{
