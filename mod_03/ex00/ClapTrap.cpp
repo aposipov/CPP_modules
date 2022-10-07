f4bc673176457e1ce9974ce8b537d25f2fe6f456
aposipov <a.p.osipov@ro.ru>
2022-10-07 17:51:18 +1000

    03

---
+++ mod_03/ex00/ClapTrap.cpp
@@ -0,0 +1,3 @@
+//
+// Created by user on 07.10.22.
+//
---
+++ mod_03/ex00/ClapTrap.hpp
@@ -0,0 +1,8 @@
+//
+// Created by user on 07.10.22.
+//
+
+#ifndef CPP_MODULES_CLAPTRAP_HPP
+#define CPP_MODULES_CLAPTRAP_HPP
+
+#endif //CPP_MODULES_CLAPTRAP_HPP
