0b36e5712fd9308af8c379fc70ca5106a43a4127
aposipov <a.p.osipov@ro.ru>
2022-10-21 21:22:44 +1000

    08 00

--- mod_08/ex00/easyfind.hpp
+++ mod_08/ex00/easyfind.hpp
@@ -8,14 +8,27 @@
 #include <vector>
 #include <exception>
 
+class FindException : public std::exception {
+	virtual const char* what() const throw() { return "Value not found"; }
+};
 
 template<typename T>
 void easyfind(T container, int val)
 {
 	if ((std::find(container.begin(), container.end(), val)) != container.end())
-		std::cout << "The val " << val << " existed" << std::endl;
+	{
+		std::cout << "The value " << val << " existed" << std::endl;
+	}
 	else
-		throw "The val dont exist";
+		throw FindException();
+}
+
+template <typename T>
+void	print_cont(T container)
+{
+	for (typename T::iterator i = container.begin(); i != container.end(); i++)
+		std::cout << *i << " ";
+	std::cout << std::endl;
 }
 
 #endif
--- mod_08/ex00/main.cpp
+++ mod_08/ex00/main.cpp
@@ -5,55 +5,67 @@
 int main()
 {
 	int arr[7] = {0, 23, 4, 56, 7, 8, 9};
+	std::cout << "-------- Test --------" << std::endl;
 	try
 	{
-		std::vector<int> MyVector(arr, arr + sizeof(arr) / sizeof(arr[0]) );
-
-		easyfind(MyVector, 23);
+		std::vector<int> myVector(arr, arr + sizeof(arr) / sizeof(arr[0]) );
+		int val = 23;
+		print_cont(myVector);
+		std::cout << "find " << val << std::endl;
+		easyfind(myVector, val);
 	}
 	catch(const std::exception& e)
 	{
-		std::cout << e.what() << '\n';
+		std::cout << e.what() << std::endl;
 	}
+	std::cout << "-------- Test --------" << std::endl;
 	try
 	{
-		std::vector<int> MyVector(arr, arr + sizeof(arr) / sizeof(arr[0]) );
-
-		easyfind(MyVector, 234);
+		std::vector<int> myVector(arr, arr + sizeof(arr) / sizeof(arr[0]) );
+		int val = 234;
+		print_cont(myVector);
+		std::cout << "find " << val << std::endl;
+		easyfind(myVector, val);
 	}
-	catch(const char *str)
+	catch(const std::exception& e)
 	{
-		std::cout << str << '\n';
+		std::cout << e.what() << std::endl;
 	}
-
+	std::cout << "-------- Test --------" << std::endl;
 	try
 	{
 		std::list<int> myList;
+		int val = 45;
 		myList.push_back(1);
 		myList.push_back(2);
 		myList.push_back(23);
 		myList.push_back(45);
 		myList.push_back(67);
-		easyfind(myList, 45);
+		print_cont(myList);
+		std::cout << "find " << val << std::endl;
+		easyfind(myList, val);
 	}
 	catch(const std::exception& e)
 	{
-		std::cout << e.what() << '\n';
+		std::cout << e.what() << std::endl;
 	}
-
+	std::cout << "-------- Test --------" << std::endl;
 	try
 	{
 		std::list<int> myList;
+		int val = 0;
 		myList.push_back(1);
 		myList.push_back(2);
 		myList.push_back(23);
 		myList.push_back(45);
 		myList.push_back(67);
-		easyfind(myList, 0);
+		print_cont(myList);
+		std::cout << "find " << val << std::endl;
+		easyfind(myList, val);
 	}
-	catch(const char *str)
+	catch(const std::exception& e)
 	{
-		std::cout << str << '\n';
+		std::cout << e.what() << std::endl;
 	}
 
 	return 0;
